2ae00540e85824cc645876364900a98a71582c25
Jessica Yang <jessicayang42@gmail.com>
2020-07-08 14:37:27 -0400

    Login testing successful

--- src/ao3_scrapy/ao3_scrapy/spiders/login_test.py
+++ src/ao3_scrapy/ao3_scrapy/spiders/login_test.py
@@ -1,27 +1,25 @@
 import scrapy
 
+def authentication_failed(response):
+    if "The password or user name you entered doesn't match our records".encode() in response.body:
+        return True
+    return False
+
 class LoginSpider(scrapy.Spider):
     name = 'login'
-
-    def authentication_failed(response):
-        pass
-
-    def start_requests(self):
-        urls = [
-            'http://www.example.com/users/login.php'
-        ]
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ['https://archiveofourown.org/users/login']
 
     def parse(self, response):
+        token = response.css('input[name=authenticity_token]::attr(value)').extract_first()
+
         return scrapy.FormRequest.from_response(
-            reponse,
+            response,
             formdata={
-                'user[name]' : 'john',
-                'user[pw]' : 'secret',
+                'user[login]': 'jeza',
+                'user[password]': 'crappypw',
+                'authenticity_token' : token
             },
-            callback=self.after_login(response)
+            callback=self.after_login
         )
 
     def after_login(self, response):
