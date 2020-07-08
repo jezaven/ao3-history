b8519caff00d26ee17338297856022518f8ac295
Jessica Yang <jessicayang42@gmail.com>
2020-07-07 23:32:51 -0400

    Changing urls so temporary parser doesn't overwrite files

--- src/ao3_scrapy/ao3_scrapy/spiders/ao3_spider.py
+++ src/ao3_scrapy/ao3_scrapy/spiders/ao3_spider.py
@@ -9,7 +9,7 @@ class WorkSpider(scrapy.Spider):
         # NOTE: temporary ao3 page before figuring out account authentication
         urls = [
             'https://archiveofourown.org/tags/The%20Good%20Place%20(TV)/works?page=1',
-            'https://archiveofourown.org/tags/The%20Good%20Place%20(TV)/works?page=2'
+            'https://archiveofourown.org/tags/%E3%83%A2%E3%83%96%E3%82%B5%E3%82%A4%E3%82%B3100%20%7C%20Mob%20Psycho%20100/works'
         ]
 
         for url in urls:
@@ -18,7 +18,7 @@ class WorkSpider(scrapy.Spider):
     # Handles response downloaded from Requests
     def parse(self, response):
         page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        filename = '%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
