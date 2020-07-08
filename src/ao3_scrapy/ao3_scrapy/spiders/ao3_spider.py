9e2e59439fb5b59d3bcaf297803290593517bcd3
Jessica Yang <jessicayang42@gmail.com>
2020-07-07 23:20:38 -0400

    Adding start_requests function

--- src/ao3_scrapy/ao3_scrapy/spiders/ao3_spider.py
+++ src/ao3_scrapy/ao3_scrapy/spiders/ao3_spider.py
@@ -1,2 +1,20 @@
 # spider to scrape from reading history
 import scrapy
+
+class WorkSpider(scrapy.Spirder):
+    name = "works"
+
+    # Returns iterable of Requests
+    def start_requests(self):
+        # NOTE: temporary ao3 page before figuring out account authentication
+        urls = [
+            'https://archiveofourown.org/tags/The%20Good%20Place%20(TV)/works?page=1',
+            'https://archiveofourown.org/tags/The%20Good%20Place%20(TV)/works?page=2'
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    # Handles response downloaded from Requests
+    def parse(self, response):
+        
