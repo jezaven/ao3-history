240f7eb4cc79fa0e312ef48ece9bfa85d3da36ab
Jessica Yang <jessicayang42@gmail.com>
2020-07-09 23:47:04 -0400

    Adding last visited information

--- src/ao3_scrapy/ao3_scrapy/spiders/ao3_spider.py
+++ src/ao3_scrapy/ao3_scrapy/spiders/ao3_spider.py
@@ -14,6 +14,49 @@ def first_login(response):
     else:
         return True
 
+# Converts string representation of month to int
+def month_convert(mon):
+    convert = {
+        'Jan' : '01',
+        'Feb' : '02',
+        'Mar' : '03',
+        'Apr' : '04',
+        'May' : '05',
+        'Jun' : '06',
+        'Jul' : '07',
+        'Aug' : '08',
+        'Sep' : '09',
+        'Oct' : '10',
+        'Nov' : '11',
+        'Dec' : '12'
+    }
+    return convert[mon]
+
+# Cleans last visited data for parse_history
+def parse_last_visited(work):
+    raw = work.css('h4.viewed::text').getall()
+    all_clean = raw[1].strip()
+    all_stats = all_clean.split('\n\n          ')
+
+    # Clean date
+    temp_date = all_stats[0].split()
+    all_stats[0] = {
+        'day' : temp_date[0],
+        'month' : month_convert(temp_date[1]),
+        'year' : temp_date[2]
+    }
+
+    # Clean version information
+    all_stats[1] = all_stats[1].strip('().')
+
+    # Clean visit count
+    try:
+        all_stats[2] = int(all_stats[2].split()[1])
+    except:
+        all_stats[2] = 1
+
+    return all_stats
+
 class HistorySpider(scrapy.Spider):
     name = "history"
 
@@ -67,34 +110,41 @@ class HistorySpider(scrapy.Spider):
     # NOTE: add gifted fic
     def parse_history(self, response):
         for work in response.xpath('//li[contains(@id, "work")]'):
+            visit = parse_last_visited(work)
+
             yield {
                 'title' : work.xpath('.//h4/a/text()').get(),
                 'author' : work.xpath('.//h4/a[@rel="author"]/text()').getall(),
                 'fandoms' : work.xpath('.//h5/a/text()').getall(),
                 'tags' : {
-                        'ratings' : work.css('ul.required-tags span.rating::attr(title)').get(),
-                        'warnings' : work.css('ul.required-tags span.warnings::attr(title)').getall(), #work.css('ul.tags li.warnings a.tag::text').getall(),
-                        # NOTE: multiple categories stored as one element
-                        'category' : work.css('ul.required-tags span.category::attr(title)').get(),
-                        'completion' : work.css('ul.required-tags span.iswip::attr(title)').get(),
-                        'relationships' : work.css('ul.tags li.relationships a.tag::text').getall(),
-                        'characters' : work.css('ul.tags li.characters a.tag::text').getall(),
-                        'freeforms' : work.css('ul.tags li.freeforms a.tag::text').getall(),
+                    'ratings' : work.css('ul.required-tags span.rating::attr(title)').get(),
+                    'warnings' : work.css('ul.required-tags span.warnings::attr(title)').getall(),
+                    # NOTE: multiple categories stored as one element
+                    'category' : work.css('ul.required-tags span.category::attr(title)').get(),
+                    'completion' : work.css('ul.required-tags span.iswip::attr(title)').get(),
+                    'relationships' : work.css('ul.tags li.relationships a.tag::text').getall(),
+                    'characters' : work.css('ul.tags li.characters a.tag::text').getall(),
+                    'freeforms' : work.css('ul.tags li.freeforms a.tag::text').getall(),
                     },
                 # NOTE: needs to be fixed, see call me beep me
-                'summary' :  work.css('blockquote.userstuff p::text').getall(),
+                'summary' :  work.css('blockquote.userstuff::text').getall(),
                 'stats' : {
-                        'language' : work.css('dl.stats dd.language::text').getall(),
-                        # NOTE: this should be reformatted to an int
-                        'word_count' : work.css('dl.stats dd.words::text').getall(),
-                        # NOTE: chapter_done and chapter_total should be reformatted together
-                        'chapter_done' : work.css('dl.stats dd.chapters a::text').getall(),
-                        'chapter_total' : work.css('dl.stats dd.chapters::text').getall(),
-                        'comments' : work.css('dl.stats dd.comments a::text').getall(),
-                        'kudos' : work.css('dl.stats dd.kudos a::text').getall(),
-                        'bookmarks' : work.css('dl.stats dd.bookmarks a::text').getall(),
-                        'hits' : work.css('dl.stats dd.hits::text').getall()
-                    }
+                    'language' : work.css('dl.stats dd.language::text').getall(),
+                    # NOTE: this should be reformatted to an int
+                    'word_count' : work.css('dl.stats dd.words::text').getall(),
+                    # NOTE: chapter_done and chapter_total should be reformatted together
+                    'chapter_done' : work.css('dl.stats dd.chapters a::text').getall(),
+                    'chapter_total' : work.css('dl.stats dd.chapters::text').getall(),
+                    'comments' : work.css('dl.stats dd.comments a::text').getall(),
+                    'kudos' : work.css('dl.stats dd.kudos a::text').getall(),
+                    'bookmarks' : work.css('dl.stats dd.bookmarks a::text').getall(),
+                    'hits' : work.css('dl.stats dd.hits::text').getall()
+                    },
+                'last_visited' : {
+                    'date' : visit[0],
+                    'version' : visit[1],
+                    'count' : visit[2]
+                }
             }
 
         next_page = response.css('li.next a::attr(href)').get()
