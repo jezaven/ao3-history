164a453e8140b35f24442163f8608f3a770641d6
Fgoyal <fgoyal2@illinois.edu>
2020-07-12 16:17:29 -0400

    Update timer

--- src/ao3_beautifulsoup/stats.py
+++ src/ao3_beautifulsoup/stats.py
@@ -5,8 +5,8 @@ import requests
 import pandas as pd
 import random
 
-start_time = time.time()
-user = User("username", "password")
+official_start_time = time.time()
+user = User("jeza", "crappypw")
 
 df = pd.DataFrame(columns=('id', 'title', 'authors', \
                            'words', 'rating', 'category', \
@@ -17,6 +17,9 @@ df = pd.DataFrame(columns=('id', 'title', 'authors', \
                            'last_updated', 'published'))
 
 count = 0
+start_time = time.time()
+f1 = open('times.txt', 'a')
+
 for work in user.reading_history():
     df.loc[count] = [work.work_id, work.title, work.authors, \
                                      work.words, work.rating, work.categories, \
@@ -26,9 +29,13 @@ for work in user.reading_history():
                                      work.bookmarks, work.hits, work.last_read, \
                                      "", ""]
     print(str(count) + ": " + work.title)
+    end_time = time.time()
+    total_time = end_time - official_start_time
+    print("--- %s seconds ---" % (total_time))
+    f1.write(str(count) + ": " + str((end_time - start_time)) + "\n")
+    start_time = end_time
+
 
     if (count % 10):
-        df.to_csv('jeza_stats.csv')
+        df.to_csv('stats.csv')
     count += 1
-
-print("--- %s seconds ---" % (time.time() - start_time))
