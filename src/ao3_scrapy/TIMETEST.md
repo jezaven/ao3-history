f2e92b9d1ac9cccc51b2805a82a28c05834f06f0
Fgoyal <fgoyal2@illinois.edu>
2020-07-13 06:31:26 -0400

    Create setters and constructor for Work object

--- src/ao3_scrapy/TIMETEST.md
+++ src/ao3_scrapy/TIMETEST.md
@@ -1,6 +1,19 @@
 # Testing Results
+
+## AutoThrottle
 | # of Pages          | With AutoThrottle   | Without AutoThrottle                                           |
 |:--------------------| --------------------| -------------------------------------------------------------- |
 | 3                   | 15.6131 seconds     | 5.2771 seconds                                                 |
 | 106                 | -                   | 130.9034 seconds (before crashing with TooManyRequests error)  |
 | 118                 | 137.4175 seconds (before crashing with TooManyRequests error) |                      |
+
+
+## Time Delay
+| Time Delay | # of Pages  |  Time (seconds) | Crashed? | Person Testing |
+|:-----------|-------------|-----------------|----------|----------------|
+| 1          | 117         | 163.5500        | Yes      | Fiza           |
+| 1          | 177         | 229.35          | Yes      | Jessica        |
+| 1.5        | 118         | 231.3712        | Yes      | Fiza           |
+| 1.5        | 118         | 227.19          | Yes      | Jessica        |
+| 2          | 120         | 297.8183        | No       | Fiza           |
+| 2          | 212         | 528.02          | Yes      | Jessica        |
--- src/ao3_scrapy/ao3_scrapy/work.py
+++ src/ao3_scrapy/ao3_scrapy/work.py
@@ -45,3 +45,107 @@ class Work(object):
     def set_id(self, id):
         self.id = id
         self.url = "https://archiveofourown.org/works/" + str(id)
+
+    # Set fic title
+    def set_title(self, title):
+        self.title = title
+
+    # Set fic authors
+    def set_authors(self, authors):
+        self.authors = authors
+
+    # Set fic gifted
+    def set_gifted(self, gifted):
+        self.gifted = gifted
+
+    # Set fic fandoms
+    def set_fandoms(self, fandoms):
+        self.fandoms = fandoms
+
+    # Set fic ratings
+    def set_ratings(self, ratings):
+        self.ratings = ratings
+
+    # Set fic warnings
+    def set_warnings(self, warnings):
+        self.warnings = warnings
+
+    # Set fic categories
+    def set_categories(self, categories):
+        self.categories = categories
+
+    # Set fic completion
+    def set_completion(self, completion):
+        self.completion = completion
+
+    # Set fic relationships
+    def set_relationships(self, relationships):
+        self.relationships = relationships
+
+    # Set fic characters
+    def set_characters(self, characters):
+        self.characters = characters
+
+    # Set fic freeforms
+    def set_freeforms(self, freeforms):
+        self.freeforms = freeforms
+
+    # Set fic series_title
+    def set_series_title(self, series_title):
+        self.series_title = series_title
+
+    # Set fic authors
+    def set_series_part(self, series_part):
+        self.series_part = series_part
+
+    # Set fic series_url
+    def set_series_url(self, series_url):
+        self.series_url = series_url
+
+    # Set fic authors
+    def set_summary(self, summary):
+        self.summary = summary
+
+    # Set fic authors
+    def set_language(self, language):
+        self.language = language
+
+    # Set fic word_count
+    def set_word_count(self, word_count):
+        self.word_count = word_count
+
+    # Set fic chapter_done
+    def set_chapter_done(self, chapter_done):
+        self.chapter_done = chapter_done
+
+    # Set fic chapter_total
+    def set_chapter_total(self, chapter_total):
+        self.chapter_total = chapter_total
+
+    # Set fic comments
+    def set_comments(self, comments):
+        self.comments = comments
+
+    # Set fic kudos
+    def set_kudos(self, kudos):
+        self.kudos = kudos
+
+    # Set fic
+    def set_bookmarks(self, bookmarks):
+        self.bookmarks = bookmarks
+
+    # Set fic hits
+    def set_hits(self, hits):
+        self.hits = hits
+
+    # Set fic last_visit_date
+    def set_last_visit_date(self, last_visit_date):
+        self.last_visit_date = last_visit_date
+
+    # Set fic last_visit_version
+    def set_last_visit_version(self, last_visit_version):
+        self.last_visit_version = last_visit_version
+
+    # Set fic last_visit_count
+    def set_last_visit_count(self, last_visit_count):
+        self.last_visit_count = last_visit_count
